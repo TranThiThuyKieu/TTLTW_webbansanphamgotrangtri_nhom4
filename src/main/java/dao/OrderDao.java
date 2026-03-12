33fbe11c5dfb7cc9254905223ab0418037e93b6d
Nguyen Minh Hau <23130096@st.hcmuaf.edu.vn>
2026-03-12 19:11:03 +0700

    refactor: tổng quan admin

--- src/main/java/dao/OrderDao.java
+++ src/main/java/dao/OrderDao.java
@@ -14,4 +14,44 @@ import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 public class OrderDao {
+    public int getOrderCount() {
+        String sql = "SELECT COUNT(*) AS cnt FROM orders WHERE payment_status = ? AND status = ?";
+
+        try (Connection conn = DBContext.getConnection();
+             PreparedStatement ps = conn.prepareStatement(sql)) {
+
+            ps.setString(1, "Đã thanh toán");
+            ps.setString(2, "Đã giao");
+
+            try (ResultSet rs = ps.executeQuery()) {
+                if (rs.next()) {
+                    return rs.getInt("cnt");
+                }
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return 0;
+    }
+    public double getTotalRevenue() {
+        String sql = "SELECT SUM(totalOrder) FROM orders WHERE payment_status = ? AND status = ?";
+
+        try (Connection conn = DBContext.getConnection();
+             PreparedStatement ps = conn.prepareStatement(sql)) {
+
+            ps.setString(1, "Đã thanh toán");
+            ps.setString(2, "Đã giao");
+
+            try (ResultSet rs = ps.executeQuery()) {
+                if (rs.next()) {
+                    return rs.getDouble(1);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return 0.0;
+    }
 }
