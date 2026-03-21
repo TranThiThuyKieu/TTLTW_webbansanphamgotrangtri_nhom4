41d342b6a6360108a1ae3e21515b33ea42a9b948
NguyenTruongGiang <23130082@st.hcmuaf.edu.vn>
2026-03-21 19:39:20 +0700

    refactor: tìm kiếm theo loại  sản phẩm

--- src/main/java/controller/SearchController.java
+++ src/main/java/controller/SearchController.java
@@ -2,11 +2,13 @@ package controller;
 
 import dao.CategoryDao;
 import dao.ProductDao;
+import dao.ProductTypeDao;
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 import model.Category;
 import model.Product;
+import model.ProductType;
 
 import java.io.IOException;
 import java.util.List;
@@ -23,16 +25,23 @@ public class SearchController extends HttpServlet {
             txtSearch = "";
         }
         String categoryIdStr = request.getParameter("categoryId");
+        String typeIdStr = request.getParameter("typeId");
         String categoryId = (categoryIdStr == null || categoryIdStr.isEmpty())
                 ? "" : categoryIdStr;
+
+        String typeId = (typeIdStr == null || typeIdStr.isEmpty())
+                ? "" : typeIdStr;
         ProductDao dao = new ProductDao();
         CategoryDao cDao = new CategoryDao();
+        ProductTypeDao tDao = new ProductTypeDao();
         List<Category> listCategory = cDao.getAllCategory();
-        List<Product> list = dao.searchProducts(txtSearch,null, categoryId);
+        List<ProductType> listType = tDao.getAll();
+        List<Product> list = dao.searchProducts(txtSearch, typeId, categoryId);
         request.setAttribute("listCC", listCategory);
         request.setAttribute("listP", list);
         request.setAttribute("txtS", txtSearch);
         request.setAttribute("selectedCategory", categoryId);
+        request.setAttribute("selectedType", typeId);
         request.getRequestDispatcher("search.jsp").forward(request, response);
     }
 
